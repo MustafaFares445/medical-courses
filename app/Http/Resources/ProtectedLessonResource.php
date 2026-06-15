888dacec6c138941ff36b849968667b4f3a3f310
Mustafa Fares <mustaphafares445@gmail.com>
2026-06-15 15:11:14 +0300

    Add protected lesson resource

---
+++ app/Http/Resources/ProtectedLessonResource.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+final class ProtectedLessonResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'summary' => $this->summary,
+            'content' => $this->content,
+            'videoUrl' => $this->video_url,
+            'courseId' => $this->section?->course_id,
+            'sectionId' => $this->course_section_id,
+            'sortOrder' => $this->sort_order,
+        ];
+    }
+}
