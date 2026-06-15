371eb9acab8b9035c047381392ed629ec3438cab
Mustafa Fares <mustaphafares445@gmail.com>
2026-06-15 14:02:08 +0300

    Add login data DTO

---
+++ app/Data/Auth/LoginData.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Data\Auth;
+
+use Spatie\LaravelData\Data;
+
+final class LoginData extends Data
+{
+    public function __construct(
+        public readonly string $email,
+        public readonly string $password,
+        public readonly string $deviceName = 'website',
+    ) {}
+}
