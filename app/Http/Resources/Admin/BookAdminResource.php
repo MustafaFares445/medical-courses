adba8c382b1afc8df04b930c0b0d5a5f3773fa51
Mustafa Fares <mustaphafares445@gmail.com>
2026-06-26 18:49:21 +0300

    Return book translations to admin

--- app/Http/Resources/Admin/BookAdminResource.php
+++ app/Http/Resources/Admin/BookAdminResource.php
@@ -7,21 +7,17 @@ namespace App\Http\Resources\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-/**
- * @mixin \App\Models\Book
- */
 final class BookAdminResource extends JsonResource
 {
-    /** @return array<string, mixed> */
     public function toArray(Request $request): array
     {
         return [
             'id' => $this->id,
             'categoryId' => $this->category_id,
-            'title' => $this->title,
+            'title' => $this->translations('title'),
             'slug' => $this->slug,
-            'shortDescription' => $this->short_description,
-            'description' => $this->description,
+            'shortDescription' => $this->translations('short_description'),
+            'description' => $this->translations('description'),
             'price' => $this->price,
             'currency' => $this->currency,
             'externalFileUrl' => $this->external_file_url,
