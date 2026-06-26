aa144923cd6cf6649cfb80317b3aba389ec33177
Mustafa Fares <mustaphafares445@gmail.com>
2026-06-26 18:52:09 +0300

    Map bilingual article data

--- app/Data/Admin/PostData.php
+++ app/Data/Admin/PostData.php
@@ -8,13 +8,12 @@ use Spatie\LaravelData\Data;
 
 final class PostData extends Data
 {
-    /** @param list<string> $fields */
     public function __construct(
         public readonly ?int $categoryId = null,
-        public readonly ?string $title = null,
+        public readonly ?array $title = null,
         public readonly ?string $slug = null,
-        public readonly ?string $excerpt = null,
-        public readonly ?string $body = null,
+        public readonly ?array $excerpt = null,
+        public readonly ?array $body = null,
         public readonly ?string $status = null,
         public readonly array $fields = [],
     ) {}
