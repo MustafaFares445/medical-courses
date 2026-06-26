98eae3515cef4e4f4469906ac10dde1d4f000398
Mustafa Fares <mustaphafares445@gmail.com>
2026-06-26 18:48:22 +0300

    Localize public section resource

--- app/Http/Resources/CourseSectionResource.php
+++ app/Http/Resources/CourseSectionResource.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Resources;
 
+use App\Support\Locale;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -11,10 +12,12 @@ final class CourseSectionResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
+        $locale = Locale::fromRequest($request);
+
         return [
             'id' => $this->id,
-            'title' => $this->title,
-            'description' => $this->description,
+            'title' => $this->localized('title', $locale),
+            'description' => $this->localized('description', $locale),
             'sortOrder' => $this->sort_order,
             'lessons' => LessonPublicResource::collection($this->whenLoaded('lessons')),
         ];
