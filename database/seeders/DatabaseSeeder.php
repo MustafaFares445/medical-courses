9963bb4433f2dd22a98dfcb8605c1bfaf7dcfbf6
Mustafa Fares <mustaphafares445@gmail.com>
2026-06-26 19:02:26 +0300

    Run demo content seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ final class DatabaseSeeder extends Seeder
         $this->call([
             AdminUserSeeder::class,
             CategorySeeder::class,
+            DemoContentSeeder::class,
         ]);
     }
 }
