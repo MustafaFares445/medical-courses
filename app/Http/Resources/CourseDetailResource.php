17b961383b089a429a3e6cb1349d2017eade8600
Mustafa Fares <mustaphafares445@gmail.com>
2026-06-26 18:47:24 +0300

    Localize public course detail resource

--- app/Http/Resources/CourseDetailResource.php
+++ app/Http/Resources/CourseDetailResource.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Resources;
 
+use App\Support\Locale;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -11,14 +12,15 @@ final class CourseDetailResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
+        $locale = Locale::fromRequest($request);
         $image = $this->getFirstMediaUrl('thumbnail') ?: null;
 
         return [
             'id' => $this->id,
-            'title' => $this->title,
+            'title' => $this->localized('title', $locale),
             'slug' => $this->slug,
-            'shortDescription' => $this->short_description,
-            'description' => $this->description,
+            'shortDescription' => $this->localized('short_description', $locale),
+            'description' => $this->localized('description', $locale),
             'price' => $this->price,
             'currency' => $this->currency,
             'category' => CategoryResource::make($this->whenLoaded('category')),
