3fb0a6d2662305b2774e9104a5d5e5dc5969bff2
Mustafa Fares <mustaphafares445@gmail.com>
2026-07-04 12:51:22 +0300

    Restore protected lesson response without disk URL lookup

--- app/Http/Resources/ProtectedLessonResource.php
+++ app/Http/Resources/ProtectedLessonResource.php
@@ -12,6 +12,21 @@ final class ProtectedLessonResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
-        return [];
+        $locale = Locale::fromRequest($request);
+
+        return [
+            'id' => $this->id,
+            'title' => $this->localized('title', $locale),
+            'slug' => $this->slug,
+            'summary' => $this->localized('summary', $locale),
+            'content' => $this->localized('content', $locale),
+            'videoUrl' => $this->video_url,
+            'videoMediaUrl' => null,
+            'courseId' => $this->section?->course_id,
+            'sectionId' => $this->course_section_id,
+            'sortOrder' => $this->sort_order,
+            'previousLesson' => null,
+            'nextLesson' => null,
+        ];
     }
 }
